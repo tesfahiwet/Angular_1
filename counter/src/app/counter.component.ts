f162679bfe389d384b772bbd453adc413cf77f0f
tesfahiwet <atesfahiwetasmelash@gmail.com>
2018-06-13 01:32:45 -0500

    Lab12

---
+++ counter/src/app/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent implements OnInit {
+
+  counterValue = 5;
+  constructor() { }
+
+  increase(){
+    this.counterValue = this.counterValue + 1;
+  }
+
+  decrease(){
+    this.counterValue = this.counterValue - 1;
+  }
+
+  ngOnInit() {
+  }
+
+}
